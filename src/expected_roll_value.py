319f194a71754844cda7d4c850593238e48ec266
root <cooperg122@gmail.com>
2025-02-25 14:39:53 -0700

    Added monte carlo for roll evs

--- src/expected_roll_value.py
+++ src/expected_roll_value.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from player import roll, score_roll
 
-def get_roll_evs(roll_probabilities: pd.DataFrame):
+def theoretic_roll_evs(roll_probabilities: pd.DataFrame):
     """Calculates the expected value of each roll based on number of dice."""
     roll_score_dict = {"Two Triples": 2500,
         "Straight": 1500,
@@ -22,5 +23,19 @@ def get_roll_evs(roll_probabilities: pd.DataFrame):
             score = roll_score_dict.get(roll_type, 0)
             ev += score * probability
         roll_evs[dice] = ev
+
+    return roll_evs
+
+def empirical_roll_evs(sims: int):
+    """Calculates estimated real evs by simulating rolls."""
+    roll_evs = pd.DataFrame(columns=range(6, 0, -1), index=["Simulated Expected Roll Value"])
+    for dice in range(6, 0, -1):
+        points = 0
+        for _ in range(sims):
+            rolled_dice = roll(dice)
+            roll_score, _ = score_roll(rolled_dice)
+            points += roll_score
+        avg_score = points / sims
+        roll_evs[dice] = avg_score
 
     return roll_evs
--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 import pandas as pd
-from player import Player
+from player import Player, roll, score_roll
 from game import Game
-from roll_probabilities import calculate_roll_probabilities, save_prob_dfs, roll_type_prob_grid, points_scoring_prob_grid
-from expected_roll_value import get_roll_evs
+from roll_probabilities import calculate_roll_probabilities, save_prob_dfs, roll_type_prob_grid, points_scoring_prob_grid, categorize_roll
+from expected_roll_value import theoretic_roll_evs, empirical_roll_evs
 
-SIMS = 20_000
+ROLL_SIMS = 1_000_000
 thresholds = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000]
 index = 4
 
 def main():
 
-    '''roll_type_prob_df, points_scoring_prob_df = calculate_roll_probabilities()
-    save_prob_dfs(roll_type_prob_df, points_scoring_prob_df)
-    '''
-    #roll_probs = pd.read_csv("Roll Type Probabilities.csv", index_col=0)
-    scoring_prob = pd.read_csv("Probability of Scoring Points.csv", index_col=0)
+    #roll_type_prob_df, points_scoring_prob_df = calculate_roll_probabilities()
+    #save_prob_dfs(roll_type_prob_df, points_scoring_prob_df)
+    
+    roll_probs = pd.read_csv("Roll Type Probabilities.csv", index_col=0)
+    #scoring_prob = pd.read_csv("Probability of Scoring Points.csv", index_col=0)
 
     #roll_type_prob_grid(roll_probs)
-    points_scoring_prob_grid(scoring_prob)
-    #get_roll_evs(roll_probs)
+    #points_scoring_prob_grid(scoring_prob)
+    roll_evs = theoretic_roll_evs(roll_probs)
+    print(roll_evs)
+    sim_roll_evs = empirical_roll_evs(1_000_000)
+    print(sim_roll_evs)
+
 
     """winners_dict = {"One": 0, "Two": 0, "Three": 0, "Four": 0, "Five": 0, "Six": 0}
     for _ in range(SIMS):
